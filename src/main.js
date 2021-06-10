5179deaa956b43730b9cddd2b37f269c95a987d7
nurvandwi <nurvandwi20@gmail.com>
2021-06-10 09:16:04 +0700

    first commit.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import VueQrcodeReader from "vue-qrcode-reader";
 
 Vue.config.productionTip = false;
+Vue.use(VueQrcodeReader);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "../components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  components: { HelloWorld },
+
+  data() {
+    return {};
   },
 };
 </script>
+
+<style scoped>
+.error {
+  font-weight: bold;
+  color: red;
+}
+</style>
