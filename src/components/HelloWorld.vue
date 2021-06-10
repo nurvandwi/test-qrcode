87c87825990253e5a6ff55f0bc5633f01f24e3d3
nurvandwi <nurvandwi20@gmail.com>
2021-06-10 09:49:05 +0700

    update 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,15 @@
 <template>
-  <div style="padding: 20px 20px 20px 20px; border: 1px solid black">
-    <div>
-      <p class="error">{{ error }}</p>
+  <div style="padding: 50px 50px 50px 50px; border: 1px solid black">
+    <p>
+      Track function:
+      <select v-model="selected">
+        <option v-for="option in options" :key="option.text" :value="option">
+          {{ option.text }}
+        </option>
+      </select>
+    </p>
 
-      <p class="decode-result">
-        Last result: <b>{{ result }}</b>
-      </p>
-
-      <qrcode-stream @decode="onDecode" @init="onInit" />
-    </div>
+    <qrcode-stream :key="_uid" :track="selected.value" @init="logErrors" />
   </div>
 </template>
 
@@ -19,34 +20,75 @@ export default {
   components: { QrcodeStream },
 
   data() {
-    return {
-      result: "",
-      error: "",
-    };
+    const options = [
+      { text: "nothing (default)", value: undefined },
+      { text: "outline", value: this.paintOutline },
+      { text: "centered text", value: this.paintCenterText },
+      { text: "bounding box", value: this.paintBoundingBox },
+    ];
+
+    const selected = options[1];
+
+    return { selected, options };
   },
+
   methods: {
-    onDecode(result) {
-      this.result = result;
+    paintOutline(detectedCodes, ctx) {
+      for (const detectedCode of detectedCodes) {
+        const [firstPoint, ...otherPoints] = detectedCode.cornerPoints;
+
+        ctx.strokeStyle = "red";
+
+        ctx.beginPath();
+        ctx.moveTo(firstPoint.x, firstPoint.y);
+        for (const { x, y } of otherPoints) {
+          ctx.lineTo(x, y);
+        }
+        ctx.lineTo(firstPoint.x, firstPoint.y);
+        ctx.closePath();
+        ctx.stroke();
+      }
     },
 
-    async onInit(promise) {
-      try {
-        await promise;
-      } catch (error) {
-        if (error.name === "NotAllowedError") {
-          this.error = "ERROR: you need to grant camera access permisson";
-        } else if (error.name === "NotFoundError") {
-          this.error = "ERROR: no camera on this device";
-        } else if (error.name === "NotSupportedError") {
-          this.error = "ERROR: secure context required (HTTPS, localhost)";
-        } else if (error.name === "NotReadableError") {
-          this.error = "ERROR: is the camera already in use?";
-        } else if (error.name === "OverconstrainedError") {
-          this.error = "ERROR: installed cameras are not suitable";
-        } else if (error.name === "StreamApiNotSupportedError") {
-          this.error = "ERROR: Stream API is not supported in this browser";
-        }
+    paintBoundingBox(detectedCodes, ctx) {
+      for (const detectedCode of detectedCodes) {
+        const {
+          boundingBox: { x, y, width, height },
+        } = detectedCode;
+
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "#007bff";
+        ctx.strokeRect(x, y, width, height);
       }
+    },
+
+    paintCenterText(detectedCodes, ctx) {
+      for (const detectedCode of detectedCodes) {
+        const { boundingBox, rawValue } = detectedCode;
+
+        const centerX = boundingBox.x + boundingBox.width / 2;
+        const centerY = boundingBox.y + boundingBox.height / 2;
+
+        const fontSize = Math.max(
+          12,
+          (50 * boundingBox.width) / ctx.canvas.width
+        );
+        console.log(boundingBox.width, ctx.canvas.width);
+
+        ctx.font = `bold ${fontSize}px sans-serif`;
+        ctx.textAlign = "center";
+
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#35495e";
+        ctx.strokeText(detectedCode.rawValue, centerX, centerY);
+
+        ctx.fillStyle = "#5cb984";
+        ctx.fillText(rawValue, centerX, centerY);
+      }
+    },
+
+    logErrors(promise) {
+      promise.catch(console.error);
     },
   },
 };
